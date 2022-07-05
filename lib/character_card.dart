023cbf9c5402ad3f06b32554de4dabb5d8a488bb
Your Name <you@example.com>
2022-07-05 18:24:39 +0800

    Add small screen support

---
+++ lib/character_card.dart
@@ -0,0 +1,188 @@
+import 'dart:math';
+
+import 'package:dartx/dartx.dart';
+import 'package:flutter/material.dart';
+
+class CharacterCardData {
+  const CharacterCardData({
+    required this.characterName,
+    required this.status,
+    required this.spice,
+    required this.lastKnownLocation,
+    required this.firstEpisodeName,
+    required this.imageURL,
+  });
+
+  final String characterName;
+  final String status;
+  final String spice;
+  final String lastKnownLocation;
+  final String firstEpisodeName;
+  final String imageURL;
+}
+
+class CharacterCard extends StatelessWidget {
+  final CharacterCardData cardData;
+  double? width;
+  double? height;
+  double? radius;
+  final BorderRadius _cardBorderRadius;
+  final BorderRadius _imageBorderRadius;
+  final bool _isHorizontal;
+
+  CharacterCard.vertical(
+      {super.key, required this.cardData, this.width, this.height, this.radius})
+      : _isHorizontal = false,
+        _imageBorderRadius = radius == null
+            ? BorderRadius.zero
+            : BorderRadius.only(
+                topLeft: Radius.circular(radius),
+                topRight: Radius.circular(radius)),
+        _cardBorderRadius = radius == null
+            ? BorderRadius.zero
+            : BorderRadius.all(Radius.circular(radius));
+
+  CharacterCard.horizontal(
+      {super.key, required this.cardData, this.width, this.height, this.radius})
+      : _isHorizontal = true,
+        _imageBorderRadius = radius == null
+            ? BorderRadius.zero
+            : BorderRadius.only(
+                topLeft: Radius.circular(radius),
+                bottomLeft: Radius.circular(radius)),
+        _cardBorderRadius = radius == null
+            ? BorderRadius.zero
+            : BorderRadius.all(Radius.circular(radius));
+
+  @override
+  Widget build(BuildContext context) {
+    final Widget content;
+    if (_isHorizontal) {
+      content = Row(
+        children: [
+          _image(),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(left: 16.0),
+              child: _body(context),
+            ),
+          ),
+        ],
+      );
+    } else {
+      content = Column(
+        children: [
+          Expanded(
+            flex: 4,
+            child: SizedBox(
+              width: double.infinity,
+              child: _image(),
+            ),
+          ),
+          Expanded(
+            flex: 3,
+            child: Padding(
+              //TODO REMOVE from here
+              padding: const EdgeInsets.only(left: 16.0),
+              child: _body(context),
+            ),
+          )
+        ],
+      );
+    }
+    return Card(
+      //TODO remove radius from here
+      shape: RoundedRectangleBorder(borderRadius: _cardBorderRadius),
+      //TODO
+      color: const Color.fromARGB(255, 60, 62, 68),
+      child: SizedBox(height: height, width: width, child: content),
+    );
+  }
+
+  ClipRRect _image() {
+    return ClipRRect(
+      borderRadius: _imageBorderRadius,
+      child: Image.network(
+        cardData.imageURL,
+        fit: BoxFit.fill,
+      ),
+    );
+  }
+
+  Column _body(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Spacer(),
+        Text(
+          cardData.characterName,
+          overflow: TextOverflow.ellipsis,
+          style: Theme.of(context).textTheme.bodyLarge,
+        ),
+        _statusLine(context),
+        const Spacer(),
+        _infoSection(
+          context,
+          title: "Last known location:",
+          body: cardData.lastKnownLocation,
+        ),
+        const Spacer(),
+        _infoSection(
+          context,
+          title: "First seen in:",
+          body: cardData.firstEpisodeName,
+        ),
+        const Spacer()
+      ],
+    );
+  }
+
+  Widget _infoSection(BuildContext context,
+      {required String title, required String body}) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          title,
+          overflow: TextOverflow.ellipsis,
+          style: Theme.of(context).textTheme.bodySmall,
+        ),
+        Text(
+          body,
+          overflow: TextOverflow.ellipsis,
+          style: Theme.of(context).textTheme.bodyMedium,
+        ),
+      ],
+    );
+  }
+
+  Row _statusLine(BuildContext context) {
+    final Color iconColor;
+    if (cardData.status == "Alive") {
+      iconColor = Colors.green;
+    } else if (cardData.status == "Dead") {
+      iconColor = Colors.red;
+    } else if (cardData.status == "unknown") {
+      iconColor = Colors.grey[400]!;
+    } else {
+      throw "unexpected error no such status: '$cardData.status'";
+    }
+
+    return Row(
+      children: [
+        Icon(
+          Icons.circle,
+          color: iconColor,
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 4),
+          child: Text(
+            "${cardData.status.capitalize()} - ${cardData.spice}",
+            overflow: TextOverflow.ellipsis,
+            style: Theme.of(context).textTheme.bodySmall,
+          ),
+        )
+      ],
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:rick_and_morty/custom_grid.dart';
 import 'package:rick_and_morty/model/character.dart';
 import 'package:rick_and_morty/model/episode.dart';
-import 'package:dartx/dartx.dart';
 import 'package:rick_and_morty/theme_data.dart';
 import 'api/api.dart';
+import 'character_card.dart';
+import 'dart:math' as math;
 
 void main(List<String> args) {
   runApp(const MyApp());
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: BrandTheme.defaultTheme,
-      home: const HomePage(),
+      home: HomePage(),
     );
   }
 }
@@ -30,125 +31,18 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class CharacterCard extends StatelessWidget {
-  const CharacterCard({
-    Key? key,
-    required this.characterName,
-    required this.status,
-    required this.spice,
-    required this.lastKnownLocation,
-    required this.firstEpisodeName,
-    required this.imageURL,
-  }) : super(key: key);
-
-  final String characterName;
-  final String status;
-  final String spice;
-  final String lastKnownLocation;
-  final String firstEpisodeName;
-  final String imageURL;
-
-  @override
-  Widget build(BuildContext context) {
-    final Color iconColor;
-    if (status == "Alive") {
-      iconColor = Colors.green;
-    } else if (status == "Dead") {
-      iconColor = Colors.red;
-    } else if (status == "unknown") {
-      iconColor = Colors.grey[400]!;
-    } else {
-      throw "unexpected error no such status: '$status'";
-    }
-
-    return Card(
-      //TODO remove radius from here
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
-      //TODO
-      color: const Color.fromARGB(255, 60, 62, 68),
-      child: Row(
-        children: [
-          ClipRRect(
-            borderRadius: const BorderRadius.only(
-              //TODO remove radius from here
-              topLeft: Radius.circular(16),
-              bottomLeft: Radius.circular(16),
-            ),
-            child: Image.network(
-              imageURL,
-              fit: BoxFit.fill,
-            ),
-          ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.only(left: 16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Spacer(),
-                  Text(
-                    characterName,
-                    overflow: TextOverflow.ellipsis,
-                    style: Theme.of(context).textTheme.bodyLarge,
-                  ),
-                  Row(
-                    children: [
-                      Icon(
-                        Icons.circle,
-                        color: iconColor,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 4),
-                        child: Text(
-                          "${status.capitalize()} - $spice",
-                          overflow: TextOverflow.ellipsis,
-                          style: Theme.of(context).textTheme.bodySmall,
-                        ),
-                      )
-                    ],
-                  ),
-                  const Spacer(),
-                  Text(
-                    "Last known location:",
-                    style: Theme.of(context).textTheme.bodySmall,
-                  ),
-                  Text(
-                    lastKnownLocation,
-                    overflow: TextOverflow.ellipsis,
-                    style: Theme.of(context).textTheme.bodyMedium,
-                  ),
-                  const Spacer(),
-                  Text(
-                    "First seen in:",
-                    style: Theme.of(context).textTheme.bodySmall,
-                  ),
-                  Text(
-                    firstEpisodeName,
-                    overflow: TextOverflow.ellipsis,
-                    style: Theme.of(context).textTheme.bodyMedium,
-                  ),
-                  const Spacer(),
-                ],
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
 class _HomePageState extends State<HomePage> {
-  var characterCards = <Widget>[];
+  var characterCardsData = <CharacterCardData>[];
   bool itemsInitialized = false;
 
   bool _loadingInPrgogress = false;
   String? _nextLoadingURL = null;
 
-  Future<CharacterCard> _loadCharacterCard(Dio dio, Character character) async {
+  Future<CharacterCardData> _loadCharacterCardData(
+      Dio dio, Character character) async {
     var episodeAnswer = await dio.get(character.episode[0]);
     var episode = Episode.fromJson(episodeAnswer.data);
-    return CharacterCard(
+    return CharacterCardData(
       characterName: character.name,
       status: character.status,
       spice: character.species,
@@ -163,11 +57,11 @@ class _HomePageState extends State<HomePage> {
     RestClient client = RestClient(dio);
     var answer = await client.getAllCharacters();
 
-    var characters = Stream.fromFutures(answer.characters
-        .map((character) => _loadCharacterCard(dio, character)));
+    var charactersData = Stream.fromFutures(answer.characters
+        .map((character) => _loadCharacterCardData(dio, character)));
 
-    await for (var character in characters) {
-      characterCards.add(character);
+    await for (var characterData in charactersData) {
+      characterCardsData.add(characterData);
     }
 
     _nextLoadingURL = answer.info.next;
@@ -192,10 +86,10 @@ class _HomePageState extends State<HomePage> {
       var allCharacterAnswer = AllCharacterAnswer.fromJson(answer.data);
 
       var characters = Stream.fromFutures(allCharacterAnswer.characters
-          .map((character) => _loadCharacterCard(dio, character)));
+          .map((character) => _loadCharacterCardData(dio, character)));
 
       await for (var character in characters) {
-        characterCards.add(character);
+        characterCardsData.add(character);
       }
 
       _nextLoadingURL = allCharacterAnswer.info.next;
@@ -208,6 +102,52 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
+    MediaQuery.of(context).size;
+    const double verticalPadding = 24;
+    const double horizontalPadding = 8;
+
+    const double bigPageCardHeight = 220;
+    const double bigPageCardWidth = 600;
+
+    const double smallPageCardHeight = 540;
+
+    const double cardRadius = 16;
+
+    const double spacing = 30;
+
+    final Widget body;
+    if (itemsInitialized) {
+      body = SliverPadding(
+        padding: const EdgeInsets.symmetric(
+          vertical: verticalPadding,
+          horizontal: horizontalPadding,
+        ),
+        sliver: MediaQuery.of(context).size.width >
+                2 * horizontalPadding + bigPageCardWidth
+            ? SliverBigPageBody(
+                cardHeight: bigPageCardHeight,
+                cardWidth: bigPageCardWidth,
+                spacing: spacing,
+                cardRadius: cardRadius,
+                characterCardsData: characterCardsData,
+              )
+            : SliverSmallPageBody(
+                cardHeight: smallPageCardHeight,
+                spacing: spacing,
+                cardRadius: cardRadius,
+                characterCardsData: characterCardsData,
+              ),
+      );
+    } else {
+      body = const SliverFillRemaining(
+        child: Center(
+          child: CircularProgressIndicator(
+            //TODO
+            color: Colors.amber,
+          ),
+        ),
+      );
+    }
     return Scaffold(
       //TODO
       backgroundColor: const Color.fromARGB(255, 32, 35, 41),
@@ -221,44 +161,111 @@ class _HomePageState extends State<HomePage> {
           return true;
         },
         child: CustomScrollView(
-          slivers: [
-            const SliverAppBar(
-              expandedHeight: 400,
-              collapsedHeight: 200,
-              flexibleSpace: Center(
-                child: Text(
-                  "The Rick and Morty",
-                  style: TextStyle(
-                      fontSize: 100,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black),
-                ),
-              ),
-              backgroundColor: Colors.white,
-            ),
-            itemsInitialized
-                ? SliverPadding(
-                    padding:
-                        const EdgeInsets.symmetric(vertical: 24, horizontal: 8),
-                    sliver: SliverGrid(
-                      delegate: SliverChildBuilderDelegate(
-                        (context, index) => characterCards[index],
-                        childCount: characterCards.length,
-                      ),
-                      gridDelegate: const CustomSliverGridDelegate(
-                          height: 220, width: 600, spacing: 30),
-                    ),
-                  )
-                : const SliverFillRemaining(
-                    child: Center(
-                      child: CircularProgressIndicator(
-                        //TODO
-                        color: Colors.amber,
-                      )
-                    ),
-                  ),
-          ],
+          slivers: [_genAppBar(), body],
         ),
+      ),
+    );
+  }
+
+  SliverAppBar _genAppBar() {
+    return const SliverAppBar(
+      expandedHeight: 400,
+      collapsedHeight: 200,
+      flexibleSpace: Center(
+        child: Text(
+          "The Rick and Morty",
+          style: TextStyle(
+              fontSize: 100, fontWeight: FontWeight.bold, color: Colors.black),
+        ),
+      ),
+      backgroundColor: Colors.white,
+    );
+  }
+}
+
+class SliverBigPageBody extends StatefulWidget {
+  const SliverBigPageBody({
+    Key? key,
+    required this.characterCardsData,
+    required this.cardHeight,
+    required this.cardWidth,
+    required this.spacing,
+    required this.cardRadius,
+  }) : super(key: key);
+
+  final double cardHeight;
+  final double cardWidth;
+  final double spacing;
+  final double cardRadius;
+
+  final List<CharacterCardData> characterCardsData;
+  @override
+  State<SliverBigPageBody> createState() => _SliverBigPageBodyState();
+}
+
+class _SliverBigPageBodyState extends State<SliverBigPageBody> {
+  @override
+  Widget build(BuildContext context) {
+    return SliverGrid(
+      delegate: SliverChildBuilderDelegate(
+        (context, index) => CharacterCard.horizontal(
+            width: widget.cardWidth,
+            height: widget.cardHeight,
+            radius: widget.cardRadius,
+            cardData: widget.characterCardsData[index]),
+        childCount: widget.characterCardsData.length,
+      ),
+      gridDelegate: CustomSliverGridDelegate(
+          height: widget.cardHeight,
+          width: widget.cardWidth,
+          spacing: widget.spacing),
+    );
+  }
+}
+
+class SliverSmallPageBody extends StatefulWidget {
+  const SliverSmallPageBody(
+      {Key? key,
+      required this.characterCardsData,
+      required this.cardHeight,
+      required this.spacing,
+      required this.cardRadius})
+      : super(key: key);
+
+  final double cardHeight;
+  final double spacing;
+  final double cardRadius;
+
+  final List<CharacterCardData> characterCardsData;
+
+  @override
+  State<SliverSmallPageBody> createState() => _SliverSmallPageBodyState();
+}
+
+class _SliverSmallPageBodyState extends State<SliverSmallPageBody> {
+  @override
+  Widget build(BuildContext context) {
+    return SliverList(
+      delegate: SliverChildBuilderDelegate(
+        (BuildContext context, int index) {
+          final int itemIndex = index ~/ 2;
+          if (index.isEven) {
+            //FIXME
+            return CharacterCard.vertical(
+              cardData: widget.characterCardsData[itemIndex],
+              height: widget.cardHeight,
+              radius: widget.cardRadius,
+            );
+          }
+          return SizedBox(height: widget.spacing);
+        },
+        semanticIndexCallback: (Widget widget, int localIndex) {
+          if (localIndex.isEven) {
+            return localIndex ~/ 2;
+          }
+          return null;
+        },
+        childCount: math.max(0, widget.characterCardsData.length * 2 - 1),
       ),
     );
   }
